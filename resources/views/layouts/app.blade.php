aac474c6dcc021e722af43410baf9d13aaa2664e
Anwar Sarmiento Ramos <anwarsarmiento@gmail.com>
2023-12-08 11:23:40 -0600

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">  <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 </head>
 <body>
     <div id="app">
@@ -71,7 +72,7 @@
             </div>
         </nav>
 
-        <main class="">
+        <main class="" style="min-height: 870px">
            <div style="display: grid; grid-template-rows: 100%; grid-template-columns: 15% 85%">
                <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px; background-color:#1d293b; ">
                    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
@@ -108,6 +109,12 @@
                @yield('content')
            </div>
         </main>
+
+        <footer class="card-footer " style="min-height: 60px; width: 100%; background-color: #1d293b; color: #f2f2f2; text-align: center; padding: 12px 0 12px 0">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Friendly Notification') }}
+            </a>
+        </footer>
     </div>
 </body>
 </html>
